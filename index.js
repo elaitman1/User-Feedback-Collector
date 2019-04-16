b8b93561dd52fa5718d184a8ab1ddf56b0ad6c8d
Eric Laitman <elaitman1@gmail.com>
2019-04-16 13:24:13 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send({hi: 'there'})
+})
+
+const PORT = process.env.PORT || 5000
+//if there is an environment var that has already been assigned to heroku, go and assign it by port, otherwise default 5000
+app.listen(PORT)
