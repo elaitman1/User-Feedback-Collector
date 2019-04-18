9d9a108cd4f94d19575c49821773c52a6bd45aaa
Eric Laitman <elaitman1@gmail.com>
2019-04-18 14:49:09 -0400

    finished auth flow

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ require('./services/passport')
 mongoose.connect(keys.mongoURI)
 
 const app = express()
-
+//app.use is middleware that is used to modify requests before they are sent to route handlers
+//used one time and is used for every request that is coming into application
+//
 app.use(
+  
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,//how long cookie can exist inside browser before it is automatically expired...must be passed in miliseconds so... 30 days 24 hours in a day 60 minutes in an hour 60 seconds in an hour 1000 miliseconds to 1 second
     keys: [keys.cookieKey] //taken from keys file
